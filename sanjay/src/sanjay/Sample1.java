6e3799349f436bf5d0b57f9e5a2921755a559033
TYSS <TYSS@TYSS-4F-053>
2018-03-09 10:59:39 +0530

    143

--- sanjay/src/sanjay/Sample1.java
+++ sanjay/src/sanjay/Sample1.java
@@ -1,5 +1,8 @@
 package sanjay;
 
-public class Sample1 {
-
+public class Sample1 
+{
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
 }
--- sanjay/src/sanjay/sample3.java
+++ sanjay/src/sanjay/sample3.java
@@ -1,8 +1,8 @@
 package sanjay;
 
-public class Sample 
-{
+public class sample3 {
 	public static void main(String[] args) {
 		System.out.println("hi");
 	}
+
 }
